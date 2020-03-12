8a7a86645ef71c17cd33d6bca553059ce7dd6c95
zidibot <gvladja@gmail.com>
2020-03-12 17:28:11 +0000

    Added comments for each subsequent script

--- R/00_doit.R
+++ R/00_doit.R
@@ -6,6 +6,7 @@ rm(list = ls())
 # ------------------------------------------------------------------------------
 library("tidyverse")
 
+
 # Define functions
 # ------------------------------------------------------------------------------
 foo <- function(x){
@@ -14,8 +15,8 @@ foo <- function(x){
 
 # Run all scripts
 # ------------------------------------------------------------------------------
-source(file = "R/01_load.R")
-source(file = "R/02_clean.R")
-source(file = "R/03_augment.R")
-source(file = "R/04_analysis_i.R")
+source(file = "R/01_load.R")        # load the data
+source(file = "R/02_clean.R")       # clean the data
+source(file = "R/03_augment.R")     # extract the necessary data
+source(file = "R/04_analysis_i.R")  # run the analysis on the augmented data
 
